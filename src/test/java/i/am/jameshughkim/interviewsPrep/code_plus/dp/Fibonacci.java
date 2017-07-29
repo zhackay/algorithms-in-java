09b6b6108ca399d30558d2e16342cad75d173009
zhac Kay <zhackay@gmail.com>
2017-07-28 17:27:04 -0700

    added fibonacci bottom up
    Took 5 minutes

--- src/test/java/i/am/jameshughkim/interviewsPrep/code_plus/dp/Fibonacci.java
+++ src/test/java/i/am/jameshughkim/interviewsPrep/code_plus/dp/Fibonacci.java
@@ -1,13 +1,32 @@
 package i.am.jameshughkim.interviewsPrep.code_plus.dp;
 
 public class Fibonacci {
+    private int[] dp;
+
+    public int fib(int n) {
+        dp = new int[n];
+
+        return fib_td(n);
+    }
+
     // 두가지 예외 F(0) = 0, F(1) = 1
-    //
-    int fib(int n) {
+    // Top Down
+    int fib_td(int n) {
         if (n <= 1) return n;
 
+        dp[n] = fib_td(n-1) + fib_td(n-2);
+        return dp[n];
+    }
+
+    int fibBu(int n) {
         int[] dp = new int[n];
-        dp[n] = fib(n-1) + fib(n-2);
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i=2;i<=n;i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         return dp[n];
     }
 }
